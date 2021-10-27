59c725b153b6524f4f9a090880f02bf3720b4313
Peter Abah <peterabah.ob@gmail.com>
2021-10-27 22:48:19 +0100

    add Misc module

--- script.js
+++ script.js
@@ -1,3 +1,41 @@
+const Misc = (function(){
+  // creates a element with tagName and properties and children
+  // example createELement(div, {class: 'big', id: '2'}) will return
+  // <div class="big", id="2"></div>
+  const createElement =  function(tagName, properties = {}, children = []) {
+    let element = document.createElement(tagName);
+
+    for(let property of Object.keys(properties)) {
+      // set the attribute of element unlesss the value is falsey
+      properties[property] && element.setAttribute(property, properties[property]);
+    }
+
+    for(let child of children) {
+      element.appendChild(child);
+    }
+
+    return element;
+  };
+
+  const create2dArray = (y, x) {
+    return array = new new Array(y).map(e => new Array(x));
+  };
+
+  const getBoardCells = function() {
+    result = create2dArray(3, 3);
+
+    for(let y = 0; y < result.length; y++) {
+      for(let x = 0; x < result[y].length; x++) {
+        result[y][x] = document.querySelector(`[data="${y} ${x}"]`);
+      }
+    }
+
+    return result;
+  };
+
+  return {create2dArray, createElement};
+})();
+
 const EventEmitter = (function() {
   let uid = -1;
   const events = {};
